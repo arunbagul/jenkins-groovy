76ace7d181e91067e9e0b262bc177ac8554fd7ec
Arun <arunbagul@gmail.com>
2022-05-06 10:11:03 +0530

    typo fixed
    typo fixed

--- howto-assign-jenkins-secret-to-global-variable.groovy
+++ howto-assign-jenkins-secret-to-global-variable.groovy
@@ -3,9 +3,6 @@
 //Plugin
 //credentials-binding ~ https://www.jenkins.io/doc/pipeline/steps/credentials-binding/
 
-//global variable
-GIT_TOKEN = ''
-
 //get secret from jenkins-secret
 node('master') {
 	withCredentials([usernamePassword(credentialsId: GIT_TOKEN_JS_ID, usernameVariable: 'gitUserName',passwordVariable: 'gtoken')]) {
